56cd38a8399a923b880a695cb6a26f475c2e84aa
Inconceivable85 <Inconceivable85@gmail.com>
2023-10-19 15:42:04 -0400

    200/404 testing.products/1,cards,couriers-expect200/PASS&NEGATIVE products/100 expect400-FAIL/BUG

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -11,3 +11,33 @@ test('Should return 200 status code....', async () => {
 	}
 	expect(actualStatusCode).toBe(200);
 });
+test('Should return 200 status code....', async () => {
+	let actualStatusCode;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/cards`);
+		actualStatusCode = response.status;
+	} catch (error) {
+		console.error(error);
+	}
+	expect(actualStatusCode).toBe(200);
+});
+test('Should return 200 status code....', async () => {
+	let actualStatusCode;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/couriers`);
+		actualStatusCode = response.status;
+	} catch (error) {
+		console.error(error);
+	}
+	expect(actualStatusCode).toBe(200);
+});
+test('Should return 404 status code....', async () => {
+	let actualStatusCode;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/products/100`);
+		actualStatusCode = response.status;
+	} catch (error) {
+		console.error(error);
+	}
+	expect(actualStatusCode).toBe(404);
+});
