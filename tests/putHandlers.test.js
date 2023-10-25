020d55fc0103d462ebcfef67fb5775fa45b0ccae
Inconceivable85 <Inconceivable85@gmail.com>
2023-10-25 15:48:41 -0400

    testing 200 and toBe = true on api/v1/kit/7

--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -1,18 +1,46 @@
-// eslint-disable-next-line no-undef
+// TESTING STATUS CODE 200
 const config = require('../config');
 
-const requestBody = {}
+const requestBody = {
+	"productsList": [
+		{
+			"id": 2,
+			"quantity": 1
+		}
+	]
+}
 
-test('', async () => {
+test('Should return Status Code 200', async () => {
+	let actualStatusCode;
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
 			method: 'PUT',
 			headers: {
 			'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(requestBody)
 		});
+		actualStatusCode = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatusCode).toBe(200);
 });
+
+//round2
+test('Response body toBe true', async () => {
+	let actualResponseBody;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
+			method:'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body:JSON.stringify(requestBody)
+		});
+		actualResponseBody = await response.json();
+	}catch (error) {
+		console.error(error);
+		}
+	expect(actualResponseBody.ok).toBe(true);
+	});
